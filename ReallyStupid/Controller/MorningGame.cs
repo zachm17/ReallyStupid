eeab3d98f81a53435c56a57d349e40ba90920225
zachm17 <zachmoy17@gmail.com>
2016-05-20 09:52:32 -0600

    Added enemy code but doesn't work

--- ReallyStupid/Controller/MorningGame.cs
+++ ReallyStupid/Controller/MorningGame.cs
@@ -5,7 +5,9 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Input;
 using ReallyStupid.Model;
-using ReallyStupid.View; 
+using ReallyStupid.View;
+using ReallyStupid.view;
+using System.Collections.Generic;
 
 
 namespace ReallyStupid.Controller
@@ -35,8 +37,19 @@ namespace ReallyStupid.Controller
 		private Texture2D mainBackground;
 
 		// Parallaxing Layers
-		private ParallaxingBackground bgLayer1;
-		private ParallaxingBackground bgLayer2;
+		 private ParallaxingBackground bgLayer1;
+		private  ParallaxingBackground bgLayer2;
+
+		// Enemies
+	private	Texture2D enemyTexture;
+		private	List<Enemy> enemies;
+
+		// The rate at which the enemies appear
+		private TimeSpan enemySpawnTime;
+		private TimeSpan previousSpawnTime;
+
+		// A random number generator
+		private Random random;
 
 		public MorningGame ()
 		{
@@ -63,7 +76,17 @@ namespace ReallyStupid.Controller
 			bgLayer1 = new ParallaxingBackground();
 			bgLayer2 = new ParallaxingBackground();
 
+			// Initialize the enemies list
+			enemies = new List<Enemy> ();
 
+			// Set the time keepers to zero
+			previousSpawnTime = TimeSpan.Zero;
+
+			// Used to determine how fast enemy respawns
+			enemySpawnTime = TimeSpan.FromSeconds(1.0f);
+
+			// Initialize our random number generator
+			random = new Random();
 
 			base.Initialize ();
 		}
@@ -89,7 +112,9 @@ namespace ReallyStupid.Controller
 			bgLayer1.Initialize(Content, "Texture/bgLayer1", GraphicsDevice.Viewport.Width, -1);
 			bgLayer2.Initialize(Content, "Texture/bgLayer2", GraphicsDevice.Viewport.Width, -2);
 
-			mainBackground = Content.Load<Texture2D>("Texture/mainbackground");
+			enemyTexture = Content.Load<Texture2D>("mineAnimation");
+
+			mainBackground = Content.Load<Texture2D>("Texture/mainBackground");
 		}
 
 		private void UpdatePlayer(GameTime gameTime)
@@ -134,6 +159,8 @@ namespace ReallyStupid.Controller
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update (GameTime gameTime)
 		{
+
+
 			// For Mobile devices, this logic will close the Game when the Back button is pressed
 			// Exit() is obsolete on iOS
 			#if !__IOS__ &&  !__TVOS__
@@ -157,8 +184,55 @@ namespace ReallyStupid.Controller
 			// Update the parallaxing background
 			bgLayer1.Update();
 			bgLayer2.Update();
+
+			// Update the enemies
+			UpdateEnemies(gameTime);
             
 			base.Update (gameTime);
+		}
+
+		private void AddEnemy()
+		{ 
+			// Create the animation object
+			Animation enemyAnimation = new Animation();
+
+			// Initialize the animation with the correct animation information
+			enemyAnimation.Initialize(enemyTexture, Vector2.Zero, 47, 61, 8, 30,Color.White, 1f, true);
+
+			// Randomly generate the position of the enemy
+			Vector2 position = new Vector2(GraphicsDevice.Viewport.Width +enemyTexture.Width / 2, random.Next(100, GraphicsDevice.Viewport.Height -100));
+
+			// Create an enemy
+			Enemy enemy = new Enemy();
+
+			// Initialize the enemy
+			enemy.Initialize(enemyAnimation, position); 
+
+			// Add the enemy to the active enemies list
+			enemies.Add(enemy);
+		}
+
+		private void UpdateEnemies(GameTime gameTime)
+		{
+			// Spawn a new enemy enemy every 1.5 seconds
+			if (gameTime.TotalGameTime - previousSpawnTime > enemySpawnTime) 
+			{
+				previousSpawnTime = gameTime.TotalGameTime;
+
+				// Add an Enemy
+				AddEnemy();
+			}
+
+			// Update the Enemies
+			for (int i = enemies.Count - 1; i >= 0; i--) 
+			{
+				enemies[i].Update(gameTime);
+
+				if (enemies[i].Active == false)
+				{
+					enemies.RemoveAt(i);
+				} 
+			}
 		}
 
 		/// <summary>
@@ -181,6 +255,12 @@ namespace ReallyStupid.Controller
 			// Draw the moving background
 			bgLayer1.Draw(spriteBatch);
 			bgLayer2.Draw(spriteBatch);
+
+			// Draw the Enemies
+			for (int i = 0; i < enemies.Count; i++)
+			{
+				enemies[i].Draw(spriteBatch);
+			}
 
 			// Draw the Player
 			player.Draw(spriteBatch);
--- ReallyStupid/View/ParallaxingBackground.cs
+++ ReallyStupid/View/ParallaxingBackground.cs
@@ -2,6 +2,9 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using ReallyStupid.Model;
+using ReallyStupid.Controller;
+
 
 namespace ReallyStupid.view
 {
